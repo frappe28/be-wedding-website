1aaebd7f03f3936944de64712f5a68b122c584ea
Francesca Deperte <francescadeperte@gmail.com>
2024-12-08 23:29:47 +0100

    sito accessibile dalla germania

--- modules/webapp/cloudfront.tf
+++ modules/webapp/cloudfront.tf
@@ -66,7 +66,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
-      locations        = ["IT", "CH"]
+      locations        = ["IT", "CH", "DE"]
     }
   }
 
